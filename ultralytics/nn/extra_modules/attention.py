d2188243f730e7f5cd69b95416c33c78c215f801
Zichen Liao <160730135+Asuka008@users.noreply.github.com>
2025-04-05 15:42:32 +0800

    Add files via upload

---
+++ ultralytics/nn/extra_modules/attention.py
@@ -0,0 +1,35 @@
+import torch
+from torch import nn
+from ..modules.conv import Conv
+
+
+__all__ = ['CAA']
+
+# CVPR2024
+class CAA(nn.Module):
+    def __init__(self, ch, h_kernel_size = 11, v_kernel_size = 11) -> None:
+        super().__init__()
+        
+        self.avg_pool = nn.AvgPool2d(7, 1, 3)
+        self.conv1 = Conv(ch, ch)
+        self.h_conv = nn.Conv2d(ch, ch, (1, h_kernel_size), 1, (0, h_kernel_size // 2), 1, ch)
+        self.v_conv = nn.Conv2d(ch, ch, (v_kernel_size, 1), 1, (v_kernel_size // 2, 0), 1, ch)
+        self.conv2 = Conv(ch, ch)
+        self.act = nn.Sigmoid()
+    
+    def forward(self, x):
+        attn_factor = self.act(self.conv2(self.v_conv(self.h_conv(self.conv1(self.avg_pool(x))))))
+        return attn_factor * x
+    
+class Mix(nn.Module):
+    def __init__(self, m=-0.80):
+        super(Mix, self).__init__()
+        w = torch.nn.Parameter(torch.FloatTensor([m]), requires_grad=True)
+        w = torch.nn.Parameter(w, requires_grad=True)
+        self.w = w
+        self.mix_block = nn.Sigmoid()
+
+    def forward(self, fea1, fea2):
+        mix_factor = self.mix_block(self.w)
+        out = fea1 * mix_factor.expand_as(fea1) + fea2 * (1 - mix_factor.expand_as(fea2))
+        return out
